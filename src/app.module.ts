66a98f6349ccfc3e810fb08022a72dc0c840d8cf
Joy-sameza <samgaming500@gmail.com>
2025-03-26 12:10:05 +0100

    Init repo

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+import { JobModule } from './job/job.module';
+import { ProposalModule } from './proposal/proposal.module';
+import { PaymentModule } from './payment/payment.module';
+import { ReviewModule } from './review/review.module';
+import { ContractModule } from './contract/contract.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    UserModule,
+    ProfileModule,
+    JobModule,
+    ProposalModule,
+    PaymentModule,
+    ReviewModule,
+    ContractModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
